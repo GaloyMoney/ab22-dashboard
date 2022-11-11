2c076ffdbc7422ba0d9d0d627119e59f78c462be
Sam Peters <unclesamtoshi@protonmail.com>
2022-11-10 20:02:13 -0600

    feat: implement dashboard design

--- config.ts
+++ config.ts
@@ -2,9 +2,12 @@ export const MERCHANT_TOKEN = process.env.MERCHANT_SESSION_TOKEN || ""
 export const GRAPHQL_URL =
   process.env.GRAPHQL_URL || "https://api.mainnet.galoy.io/graphql"
 export const MEMO_TO_MERCHANT: Record<string, string> = {
-  AB22Kebab: "Kebab",
+  AB22Kebabs: "Kebab",
   AB22Swag: "Swag",
-  AB22Burger: "Burger",
+  AB22Burgers: "Burger",
+  AB22Ceviche: "Ceviche",
+  AB22Pupusas: "Pupusa",
+  AB22Wraps: "Wraps",
 }
 export const CONFERENCE_START = new Date(
   process.env.CONFERENCE_START || "2022-10-03T00:00:00",
--- hooks/use-fetch.tsx
+++ hooks/use-fetch.tsx
@@ -1,8 +1,13 @@
 import useSWR from "swr"
+import { PaymentStatsSummary } from "../pages/api/stats"
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-export default function useFetchData() {
+export default function useFetchData(): {
+  data: PaymentStatsSummary
+  isLoading: boolean
+  isError: any
+} {
   const { data, error } = useSWR(`/api/stats`, fetcher, { refreshInterval: 30 * 1000 })
 
   return {
