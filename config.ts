bf918227a3e32741cc4050be4f122ebb5c46fa07
UncleSamtoshi <88598461+UncleSamtoshi@users.noreply.github.com>
2022-11-13 14:35:35 -0800

    chore: change ratio for conference screen (#7)
    * chore: change ratio for conference screen
    
    * chore: change styling for tall screen

--- config.ts
+++ config.ts
@@ -2,12 +2,13 @@ export const MERCHANT_TOKEN = process.env.MERCHANT_SESSION_TOKEN || ""
 export const GRAPHQL_URL =
   process.env.GRAPHQL_URL || "https://api.mainnet.galoy.io/graphql"
 export const MEMO_TO_MERCHANT: Record<string, string> = {
-  AB22Kebabs: "Kebab",
+  AB22Burgers: "Burgers",
+  AB22Ceviches: "Ceviches",
   AB22Swag: "Swag",
-  AB22Burgers: "Burger",
-  AB22Ceviche: "Ceviche",
-  AB22Pupusas: "Pupusa",
+  AB22Pupusas: "Pupusas",
   AB22Wraps: "Wraps",
+  AB22Bar1: "Bar 1",
+  AB22Bar2: "Bar 2"
 }
 export const CONFERENCE_START = new Date(
   process.env.CONFERENCE_START || "2022-10-03T00:00:00",
